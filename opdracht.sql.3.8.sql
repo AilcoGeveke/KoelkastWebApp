559a027283a74c58a723aa5df0c2da86945dc933
ritseart <ritseart@hotmail.nl>
2016-03-08 11:12:27 +0100

    Deleted

--- opdracht.sql.3.8.sql
+++
@@ -1,8 +0,0 @@
-SELECT pres_hob.pres_name, COUNT(pres_hob.pres_name) AS nr_hobbies 
-FROM pres_hob 
-GROUP BY pres_hob.pres_name
-HAVING COUNT(pres_hob.pres_name) = 
-	(SELECT COUNT(pres_hob.pres_name) 
-	FROM pres_hob 
-	WHERE pres_hob.pres_name = '"Roosevelt F D"')
-;
